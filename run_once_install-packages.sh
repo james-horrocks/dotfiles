661bb4ee0fa195c6cec06efa77fdfc3ab8775991
James Horrocks <james.horrocks@live.co.uk>
2020-11-09 20:11:51 +0000

    added run_once_install-packages.sh

---
+++ run_once_install-packages.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Installed:
+
+# zsh
+# oh-my-zsh
+# alacritty
+# regolith
+# polybar
+# glava
+# cli-visualizer
+# spicetify
+
+# fonts in folder +...
+# fonts-font-awesome
+# fonts-materialdesignicons-webfont
+# fonts-source-code-pro-ttf
+
+# see setup.sh in Dropbox
