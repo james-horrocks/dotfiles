3208839bec52c78a94b7d4a51e295894b656d748
James Horrocks <james.horrocks@zoopla.co.uk>
2021-02-16 17:45:39 +0000

    consolidated aliases and removed archlinux zsh plugin

--- dot_oh-my-zsh/custom/aliases.zsh
+++ dot_oh-my-zsh/custom/aliases.zsh
@@ -1,13 +1,17 @@
+alias homeserver="ssh james@homeserver"
 alias zshconfig="nano ~/.zshrc"
 alias jetwork="ssh root@jetwork.lan"
 alias venv="source venv/bin/activate"
 alias python=python3
 alias pip=pip3
 alias sshadmin="ssh -A dev-jhorrocks -t 'ssh -A admin@release-001.admin.uk'"
-alias azkaban="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk build-build-001.data-warehouse.uk"
-alias azkaban2="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk build-build-azkaban-001.data-warehouse.uk"alias homeserver="ssh james@homeserver"
-alias azkaban2="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk build-build-azkaban-001.data-warehouse.uk"
-alias azkaban-web="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk build-build-azkaban-web-001.data-warehouse.uk"
+alias azkaban="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk ec2-user-build-001.data-warehouse.uk"
+alias azkaban2="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk ec2-user-build-azkaban-001.data-warehouse.uk"
+alias azkaban-web="ssh -t dev-jhorrocks ssh -t admin@release-001.admin.uk ec2-user-build-azkaban-web-001.data-warehouse.uk"
 alias chezs="chezmoi git status"
 alias chezc="chezmoi git add . && chezmoi git -- commit -m"
 alias chezp="chezmoi git push"
+alias data-qa="aws-vault exec qa -- "
+alias data-prod="aws-vault exec prod -- "
+alias data-dev="aws-vault exec dev -- "
+alias zeppelin=/home/james/.local/share/zeppelin-0.8.1/bin/zeppelin-daemon.sh
